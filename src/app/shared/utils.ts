003fd165593c13de110e8316b46546b05c4b6f8d
Edgar Durand <edgarduranddiaz@gmail.com>
2021-09-09 23:26:59 -0400

    posts

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -5,7 +5,7 @@ import {MatTableDataSource} from "@angular/material/table";
  * set columns from object's keys
  * @param object
  */
-export const getColumns = (object: any): string[] => {
+export const getColumns = <T>(object: T): string[] => {
   const columns: string[] = [];
   Object.keys(object).forEach((key) => columns.push(key))
   return columns;
@@ -16,7 +16,7 @@ export const getColumns = (object: any): string[] => {
  * @param filterValue
  * @param datasource
  */
-export const applyFilter = (filterValue: string, datasource: MatTableDataSource<any>): void => {
+export const applyFilter = <T>(filterValue: string, datasource: MatTableDataSource<T>): void => {
   if (filterValue) {
     datasource.filter = filterValue.trim().toLowerCase();
   } else {
